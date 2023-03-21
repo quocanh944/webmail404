79bc279e3024647c9ac93717e794873a387424b2
unknown <quocanh944@gmail.com>
2023-03-21 09:56:06 +0700

    Add SQL and change tailwind to bootstrap

--- views/partials/head.php
+++ views/partials/head.php
@@ -1,10 +1,12 @@
 <!doctype html>
 <html lang="en" class="h-full bg-gray-100">
+
 <head>
     <meta charset="UTF-8">
     <title>Demo</title>
-    <script src="https://cdn.tailwindcss.com?plugins=forms"></script>
-</head>
+    <!-- <script src="https://cdn.tailwindcss.com?plugins=forms"></script> -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.0.1/js/bootstrap.min.js" integrity="sha512-EKWWs1ZcA2ZY9lbLISPz8aGR2+L7JVYqBAYTq5AXgBkSjRSuQEGqWx8R1zAX16KdXPaCjOCaKE8MCpU0wcHlHA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.0.1/css/bootstrap.min.css" integrity="sha512-Ez0cGzNzHR1tYAv56860NLspgUGuQw16GiOOp/I2LuTmpSK9xDXlgJz3XN4cnpXWDmkNBKXR/VDMTCnAaEooxA==" crossorigin="anonymous" referrerpolicy="no-referrer" /></head>
 
 <body class="h-full">
     <div class="min-h-full">
