44176ab9670be470eefe557efb6d545d241016a5
Võ Phú Vinh <52100947@student.tdtu.edu.vn>
2023-04-17 13:04:54 +0700

    commit

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ return [
     'database' => [
         'host' => 'localhost',
         'port' => 3306,
-        'dbname' => '404webmail_new',
+        'dbname' => '404webmail_newdata6',
         'charset' => 'utf8mb4'
     ],
 ];
